518254621824ea225b3e1c2c6a5015595ee3a690
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2023-07-31 10:46:23 +0530

    go

--- index.js
+++ index.js
@@ -505,7 +505,7 @@ const fuseOptions = {
   findAllMatches: true,
   // minMatchCharLength: 1,
   // location: 0,
-  // threshold: 0.6,
+  threshold: 0.3,
   // distance: 100,
   // useExtendedSearch: false,
   // ignoreLocation: false,
